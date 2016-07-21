110d872762bf1e56ce15a17113958cafd0f88aa4
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-21 14:09:33 +0100

    fixed https error

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <script>
 function onSignIn(googleUser) {
