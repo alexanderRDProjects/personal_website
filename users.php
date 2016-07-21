195330a7dd9540c86350770f02b77793d2a7d35f
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-21 14:36:55 +0100

    setting up pg

--- users.php
+++ users.php
@@ -1,14 +1,19 @@
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+<?php 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+$host = "localhost"; 
+$user = "user12"; 
+$pass = "34klq*"; 
+$db = "testdb"; 
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+    or die ("Could not connect to server\n"); 
+
+$query = "SELECT VERSION()"; 
+$rs = pg_query($con, $query) or die("Cannot execute query: $query\n"); 
+$row = pg_fetch_row($rs);
+
+echo $row[0] . "\n";
+
+pg_close($con); 
+
 ?>
