7e47f5ab67def16159b34f240f689cea5c0df460
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-21 12:49:32 +0100

    creating database

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+</head>
+<body>
 <?php 
-echo phpinfo() ?>
+pg_query("CREATE DATABASE projects")
+?>
+</body>
+</html>
