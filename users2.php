5d8c5eec698fc2a5fd6aa4f8c8c292b3207a053e
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-21 15:58:21 +0100

    uses external website

--- users2.php
+++ users2.php
@@ -1,28 +1,10 @@
-<?php 
+<?php
+// Create connection
+$conn = new mysqli("sql8.freemysqlhosting.net", "sql8128538", "823TWl4xY9",3306);
 
-$host = "ec2-54-217-243-228.eu-west-1.compute.amazonaws.com"; 
-$user = "shozqbxwvyjkrp"; 
-$pass = "5YW1EECQcJb5-9RGbr4gHX9_Pz"; 
-$db = "d5rp8t24likiqk"; 
-$port = 5432;
-
-$con = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp password=5YW1EECQcJb5-9RGbr4gHX9_Pz port=5432")
-    or die ("Could not connect to server\n"); 
-
-$query = <<<EOF
-      CREATE TABLE USER
-      (ID INT PRIMARY KEY     NOT NULL,
-	  GKEY           TEXT     NOT NULL,
-      NAME           TEXT     NOT NULL,
-      EMAIL          TEXT     NOT NULL);
-EOF;
-$ret = pg_query($con, $sql);
-   if(!$ret){
-      echo pg_last_error($con);
-   } else {
-      echo "Table created successfully\n";
-   }
-pg_close($con);
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
-stuff
