5c4c78e5d93b9c9e0be0e53132012ca203a56f17
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-21 15:01:48 +0100

    fixed 500

--- users2.php
+++ users2.php
@@ -4,7 +4,7 @@ $host = "ec2-54-217-243-228.eu-west-1.compute.amazonaws.com";
 $user = "shozqbxwvyjkrp"; 
 $pass = "5YW1EECQcJb5-9RGbr4gHX9_Pz"; 
 $db = "d5rp8t24likiqk"; 
-$port = 5432
+$port = 5432;
 
 $con = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp password=5YW1EECQcJb5-9RGbr4gHX9_Pz port=5432")
     or die ("Could not connect to server\n"); 
