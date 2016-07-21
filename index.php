bc647da5886fe252928a7aa1cdfe475736897719
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-21 14:04:21 +0100

    setting up database

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="434726568124-obcv7f1fmpqse2gard1nh55513av5vff.apps.googleusercontent.com">
 </head>
 <body>
-<p>also <?php 
-echo pg_query("CREATE DATABASE projects")
-?></p>
-</html>
+<p>sign in </p>
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
 </body>
+</html>
