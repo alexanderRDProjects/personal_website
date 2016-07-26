d13a00819758b3f6ff8cfe04e1ca2beca3964d34
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-26 17:10:54 +0100

    fixing home links

--- index.php
+++ index.php
@@ -31,10 +31,10 @@ function signOut() {
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Alexander Roberts Dunn Projects</a>
+      <a class="navbar-brand" href="/">Alexander Roberts Dunn Projects</a>
     </div>
     <ul class="nav navbar-nav">
-		<li class="active"><a href="#">Home</a></li>
+		<li class="active"><a href="/">Home</a></li>
 		<li><a href = "/about/">About</a></li>
 		<li><a href = "/projects/view/">Projects</a></li>
 		<li><a href = "/projects/">Random Project</a></li>
