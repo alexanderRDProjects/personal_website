6cc1d1cfd274b212c7a5165f4de7a8e33611bf96
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-26 17:42:30 +0100

    removed angular

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -36,7 +35,7 @@ function signOut() {
     <ul class="nav navbar-nav">
 		<li class="active"><a href="/">Home</a></li>
 		<li><a href = "/about/">About</a></li>
-		<li><a href = "/projects/">Projects</a></li>
+		<li><a href = "/projects/">Projects</a></li>	
 		<li><a href = "/projects/random/">Random Project</a></li>
     </ul>
 	<ul class="nav navbar-nav navbar-right">
