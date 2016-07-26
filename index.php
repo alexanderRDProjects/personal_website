581996ef58bedbd3b6cc5770a12ab40fb0479264
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-26 17:28:06 +0100

    changed structure

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ function signOut() {
 <meta name="google-signin-client_id" content="434726568124-obcv7f1fmpqse2gard1nh55513av5vff.apps.googleusercontent.com">
 </head>
 <body>
+<div class="jumbotron text-center">
 <span class = "container-fluid">
 <nav class="navbar navbar-default">
   <div class="container-fluid">
@@ -44,7 +45,6 @@ function signOut() {
 	</ul>
   </div>
 </nav>
-<div class="jumbotron text-center">
 <h1> Coding, Web Design and More </h1>
 <p>where it all happens</p>
 </div>
