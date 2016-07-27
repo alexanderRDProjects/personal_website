d06b259ccd268aeece9e610782f7e6b3df126d71
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-27 18:27:01 +0100

    dodgy fix

--- manage.php
+++ manage.php
@@ -3,11 +3,6 @@ $duck = $_GET["duck"];
 if ($duck == "true"){
 	echo "hello ducky user";
 }else {
-	echo "<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /manage.php was not found on this server.</p>
-</body></html>";
+	echo "<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL /manage.php was not found on this server.</p></body></html>";
 }
 ?>
