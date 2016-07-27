fa4b6aabac7f12553f3b294f1c89a4cf2d6772a8
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-27 18:52:32 +0100

    back end work

--- manage.php
+++ manage.php
@@ -9,8 +9,8 @@ if ($duck == "true"){
 }
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
 $sql =<<<EOF
-      SELECT *
-	FROM usercomments
+      select column_name, data_type, character_maximum_length
+from INFORMATION_SCHEMA.COLUMNS where table_name = 'usercomments'; 
 EOF;
 
    $ret = pg_query($db, $sql);
