39fc70598b8831ad652d4066556f7f9331ba51b7
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-27 20:48:56 +0100

    back end work

--- projects/this/new.php
+++ projects/this/new.php
@@ -5,7 +5,7 @@ echo $name." says ".$comment;
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
 $sql =<<<EOF
 	insert into usercomments (GoogleID,Name,Comment,ProjectName)
-	values (1,$name,$comment,'website dev');
+	values (1,'$name','$comment','website dev');
 EOF;
 
    $ret = pg_query($db, $sql);
