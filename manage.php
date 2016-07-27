4291c0d8d343c5a37131e497878ffcd3f158c301
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-27 18:44:00 +0100

    back end work

--- manage.php
+++ manage.php
@@ -9,7 +9,9 @@ if ($duck == "true"){
 }
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
 $sql =<<<EOF
-      dt;
+      SELECT table_schema,table_name
+	FROM information_schema.tables
+	ORDER BY table_schema,table_name;
 EOF;
 
    $ret = pg_query($db, $sql);
