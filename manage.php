4e61a34cab5edf6e4bc0698f7a4a14811134a14e
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-27 18:34:47 +0100

    back end work

--- manage.php
+++ manage.php
@@ -5,5 +5,18 @@ if ($duck == "true"){
 	echo "hello ducky user";
 }else {
 	echo "<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL /manage.php was not found on this server.</p></body></html>";
+	die();
 }
+$sql =<<<EOF
+      \dt;
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+      exit;
+   } 
+   echo $ret
+   echo "Operation done successfully\n";
+   pg_close($db);
 ?>
