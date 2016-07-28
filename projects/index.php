9e1fcdc31bd5811615de650c3e6e082c02059477
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-28 18:46:32 +0100

    added background

--- projects/index.php
+++ projects/index.php
@@ -48,5 +48,8 @@ function signOut() {
 <h1> Projects </h1>
 </div>
 </span>
+<div style='position:absolute;z-index:-15;left:0;top:0;width:100%;height:100%'>
+  <img src='projects.jpg' style='width:100%;height:100%' alt='[]' />
+</div>
 </body>
 </html>
