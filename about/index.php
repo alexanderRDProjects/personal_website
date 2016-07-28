8f7ca5565a3a2de9b75c8acf27d07bafb24bc788
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-28 21:12:27 +0100

    added background

--- about/index.php
+++ about/index.php
@@ -48,6 +48,17 @@ function signOut() {
     background-color: rgba(238,238,238,0.5);
 ">
 <h1> About </h1>
+<div class="row">
+    <div class="col-sm-6">
+	<p>descritpion of me and the stuff i am doing and other stuff like that</p>
+	</div>
+	<div class="col-sm-6">
+	<img src="me.png" style="
+    width: 100%;
+    /* height: 100%; */
+">
+	</div>
+</div>
 </div>
 </span>
 <div style='position:absolute;z-index:-15;left:0;top:0;width:100%;height:100%'>
