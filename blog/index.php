80648c1594e88d474a963edc85c9d09dc4ab7205
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-31 16:33:01 +0100

    user login

--- blog/index.php
+++ blog/index.php
@@ -50,5 +50,8 @@ function signOut() {
 <p>short_description<p>
 </div>
 </span>
+<div style='position:absolute;z-index:-15;left:0;top:0;width:100%;height:100%'>
+  <img src='projects.jpg' style='width:100%;height:100%' alt='[]' />
+</div>
 </body>
 </html>
--- userlogin.php
+++ userlogin.php
@@ -1,1 +1,11 @@
 <?php
+$db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
+$sql = "SELECT TOP 1 userdetails.id FROM products WHERE userdetails.id = ".$username.";"
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+      exit;
+   } 
+   var_dump(pg_fetch_all($ret));
+?>
