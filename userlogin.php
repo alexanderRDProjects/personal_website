ece6df29f0fbb333a1e74f43b87d49c4b2e8da40
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-31 16:38:17 +0100

    fix 500

--- userlogin.php
+++ userlogin.php
@@ -1,4 +1,5 @@
 <?php
+$username = $_GET["username"];
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
 $sql = "SELECT TOP 1 userdetails.id FROM products WHERE userdetails.id = ".$username.";"
 
