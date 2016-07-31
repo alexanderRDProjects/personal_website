afcd021c3d42e00f86943695697326aa1ead1083
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-31 16:40:33 +0100

    selected right field

--- userlogin.php
+++ userlogin.php
@@ -1,7 +1,7 @@
 <?php
 $username = $_GET["username"];
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
-$sql = "SELECT TOP 1 userdetails.id FROM products WHERE userdetails.id = ".$username.";"
+$sql = "SELECT TOP 1 userdetails.GoogleID FROM products WHERE userdetails.GoogleID = ".$username.";"
 
    $ret = pg_query($db, $sql);
    if(!$ret){
