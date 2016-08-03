5b0c37663cb043861f3010259f9e399379bf02d2
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-08-03 21:49:02 +0100

    another gommit

--- userlogin.php
+++ userlogin.php
@@ -4,17 +4,11 @@ $name = $_GET["Name"];
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
 $sql = "SELECT GoogleID FROM userdetails WHERE GoogleID = '".$googleID."';";
 $ret = pg_query($db, $sql);
-if(!$ret){
-	echo pg_last_error($db);
-	exit;
-} 
 if (var_dump(pg_fetch_all($ret)) == false){
 	// get user details
 	$sqli = "INSERT INTO userdetails(name,googleid) VALUES ('".$name."','".$googleID."');";
+	echo $sqli;
 	$reti = pg_query($db,$sqli);
-	if(!$reti){
-		echo pg_last_error($db);
-		exit;
-	} 
+	echo "success";
 }
 ?>
