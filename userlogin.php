50293d1d994c4c7b6479e819f172598c6992c5c4
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-08-03 21:37:28 +0100

    quotes should be right now (i hope)

--- userlogin.php
+++ userlogin.php
@@ -1,7 +1,7 @@
 <?php
 $username = $_GET["username"];
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
-$sql = "SELECT GoogleID FROM userdetails WHERE GoogleID = 'cool';';
+$sql = "SELECT GoogleID FROM userdetails WHERE GoogleID = 'cool';";
 $ret = pg_query($db, $sql);
 if(!$ret){
 	echo pg_last_error($db);
