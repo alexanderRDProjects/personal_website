3a9895cf2d7f8b0171eec0653d6577653b4c7af5
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-08-03 21:49:57 +0100

    forget to git

--- userlogin.php
+++ userlogin.php
@@ -4,6 +4,10 @@ $name = $_GET["Name"];
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
 $sql = "SELECT GoogleID FROM userdetails WHERE GoogleID = '".$googleID."';";
 $ret = pg_query($db, $sql);
+if(!$ret){
+	echo pg_last_error($db);
+	exit;
+} 
 if (var_dump(pg_fetch_all($ret)) == false){
 	// get user details
 	$sqli = "INSERT INTO userdetails(name,googleid) VALUES ('".$name."','".$googleID."');";
