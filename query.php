8bd2919563c913c61d624e66cf43f6926fda5513
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-08-06 21:21:12 +0100

    add query tool

---
+++ query.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_GET["name"];
+$comment = $_GET["comment"];
+echo $name." says ".$comment;
+$db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
+$sql = "select * from usercomments";
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+      exit;
+   } 
+   echo var_dump($ret);
+   echo "Operation done successfully\n";
+   var_dump(pg_fetch_all($ret));
+   pg_close($db); 
+?>
