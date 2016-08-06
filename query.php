2d65acaf19bacb72112653933168fe38aa039ec3
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-08-06 21:52:33 +0100

    query

--- query.php
+++ query.php
@@ -1,6 +1,6 @@
 <?php
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
-$sql = "SELECT * from userdetails";
+$sql = "SELECT * from projects";
 
    $ret = pg_query($db, $sql);
    if(!$ret){
