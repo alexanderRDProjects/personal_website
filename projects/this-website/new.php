172b818eac847b268029d158838a2b64348b8ae5
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-08-06 21:38:34 +0100

    complete query

--- projects/this-website/new.php
+++ projects/this-website/new.php
@@ -2,18 +2,18 @@
 $name = $_GET["name"];
 $comment = $_GET["comment"];
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
-$sql =<<<EOF
-	insert into usercomments (GoogleID,Name,Comment,ProjectName)
-	values (1,'$name','$comment','website dev');
-EOF;
+
 $query = "select userID from userdetails where name = '$name'";
 $res = pg_query($db,$query);
 
 echo "<p>".$name." says ".$comment."</p>";
 echo "<p> finding userID </p>";
 echo var_dump(pg_fetch_all($res));
-
-
+$userid = pg_fetch_all($res)["userid"];
+$sql =<<<EOF
+	insert into usercomments (userID,Comment,ProjectName)
+	values ('$userid','$comment','website dev');
+EOF;
 
    $ret = pg_query($db, $sql);
    if(!$ret){
