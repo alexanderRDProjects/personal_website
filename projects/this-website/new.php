4d7b73611c0ebb28b5cd2539d100ec89106a834a
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-08-06 21:48:05 +0100

    finding projectid

--- projects/this-website/new.php
+++ projects/this-website/new.php
@@ -2,7 +2,7 @@
 $name = $_GET["name"];
 $comment = $_GET["comment"];
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
-
+//userid
 $query = "select userID from userdetails where name = '$name'";
 $res = pg_query($db,$query);
 
@@ -11,6 +11,15 @@ echo "<p> finding userID </p>";
 echo var_dump(pg_fetch_all($res));
 $userid = pg_fetch_all($res)[0]["userid"];
 echo "<p>userid is".$userid."</p>";
+//project id
+$pquery = "select projectid from projects where ProjectName = 'website dev'";
+$pres = pg_query($db,$pquery);
+
+echo "<p>".$name." says ".$comment."</p>";
+echo "<p> finding projectID </p>";
+echo var_dump(pg_fetch_all($pres));
+$projectid = pg_fetch_all($pres)[0]["projectid"];
+echo "<p>projectid is".$projectid."</p>";
 $sql =<<<EOF
 	insert into usercomments (userID,Comment,ProjectId)
 	values ($userid,'$comment','website dev');
