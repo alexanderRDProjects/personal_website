607b8748798f20d7ee95a4aff58c9bb2fcd8de43
Alexander <a.rdunn.cs@gmail.com>
2016-11-16 14:55:36 +0000

    encoded name as raw url

--- userlogin.php
+++ userlogin.php
@@ -3,6 +3,7 @@ $googleID = $_GET["googleID"];
 $name = $_GET["Name"];
 $url = $_GET["url"];
 echo $googleID.$name.$url;
+$name = rawurlencode($name);
 $db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
 $sql = "SELECT GoogleID FROM userdetails WHERE GoogleID = '".$googleID."';";
 $ret = pg_query($db, $sql);
