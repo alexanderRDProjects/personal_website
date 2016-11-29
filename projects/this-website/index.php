6fb414b7d8a928033ee6f735f349014a3850406d
Alexander <a.rdunn.cs@gmail.com>
2016-11-29 22:21:42 +0000

    adding pictures

--- projects/this-website/index.php
+++ projects/this-website/index.php
@@ -94,7 +94,7 @@ while($row = pg_fetch_row($ret)){
       echo '<div class="well-sm" style="
     padding-bottom: 0px;
 "> <p style="margin-bottom: 0px;"><strong style="font-size: 16px;
-">'. pg_fetch_all($rete)[0]["name"] . '</strong></p><p style="
+"><img href="'.pg_fetch_all($rete)[0]["url"]).'"/>'. pg_fetch_all($rete)[0]["name"] . '</strong></p><p style="
     margin-bottom: 0px;
 ">';
       echo '<p style="
