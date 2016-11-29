abfc22eec3b84078930a42ecebb86c7c13ae1316
Alexander <a.rdunn.cs@gmail.com>
2016-11-29 21:48:41 +0000

    impletementing the blog system

--- blog/index.php
+++ blog/index.php
@@ -1,3 +1,18 @@
+<?php
+$db = pg_connect("host=ec2-54-217-243-228.eu-west-1.compute.amazonaws.com dbname=d5rp8t24likiqk user=shozqbxwvyjkrp port=5432 password=5YW1EECQcJb5-9RGbr4gHX9_Pz");
+$sql =<<<EOF
+	select * from blog
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+      exit;
+   } 
+   echo $ret;
+   //echo "Operation done successfully\n";
+   
+?>
 <!DOCTYPE html>
 <html>
 <head>
