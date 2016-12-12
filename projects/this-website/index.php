9fcf63d8b3bb62fe2dcdbb1f10d1d7909bc5db29
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2016-12-12 23:31:42 +0000

    added missing semicolon

--- projects/this-website/index.php
+++ projects/this-website/index.php
@@ -122,7 +122,7 @@ foreach ($comments as &$comment)
 {
 	echo "<tr><td><br></td></tr>";
 	echo "<tr><td><img src='".$comment[1]."'/></td>";
-	echo "<table><tr><td>".str_replace("%20"," ",$comment[0])."</td></tr>"
+	echo "<table><tr><td>".str_replace("%20"," ",$comment[0])."</td></tr>";
 	echo "<tr><td>".$comment[2]."</td></tr></table></tr>";
 	//echo "<tr><td>".str_replace("%20"," ",$comment[0])."</td>";
 	//echo "<tr><td><img src='".$comment[1]."'/></td><td>".$comment[2]."</td></tr>";
