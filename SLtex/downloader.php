29e96607ec7cc5bccdc7ada6e0163de9f7a9cff8
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 01:56:37 +0000

    stil fixing SLtex

--- SLtex/downloader.php
+++ SLtex/downloader.php
@@ -1,17 +1,13 @@
 <?php
 //echo 'hello wolrd';
-$x = 'example.tex';
-if (file_exists($x)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($x).'"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($x));
-    readfile($x);
-    exit;
-}
-
+$x = "jsdfdflkjdf"
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'.basename($x).'"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($x));
 echo $x;
+exit;
 ?>
