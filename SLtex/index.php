2eaf0e37a8003ef094f1e547f803c7868eea122b
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 18:41:45 +0000

    added front end

---
+++ SLtex/index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>SLtex Converter</title>
+</head>
+<body>
+<table>
+<tr>
+<td>
+<center><h1></h1></center>
+</td>
+</tr>
+<tr>
+<td> SLTex </td>
+<td> LaTex </td>
+</tr>
+<tr>
+<td>
+<textarea id = "SLText">
+Enter Your SLTex here ...
+</textarea>
+</td>
+<td>
+<textarea id = "SLText">
+Enter Your SLTex here ...
+</textarea>
+</td>
+</tr>
+<tr>
+<td><button>Convert To LaTex</button></td>
+<td><button>Download LaTex </button></td>
+<td><button>Convert TO SLTex </button></td>
+<td><button>Download SLTex </button></td>
+</tr>
+</table>
+</body>
+</html>
