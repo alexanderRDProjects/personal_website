c0677441f051386e3c609b5d6ef5cf96c8461397
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 20:56:54 +0000

    expanding

--- SLtex/convert.php
+++ SLtex/convert.php
@@ -4,13 +4,26 @@ $output = [];
 $input = array_filter(explode("\n",$content));
 var_dump($input);
 foreach($input as &$line) {
-	echo "<p> unformatted line :".$line."</p>";
+	//echo "<p> unformatted line :".$line."</p>";
 	$line = str_replace("pi","\pi",$line);
 	$line = str_replace("phi","\phi",$line);
 	$line = str_replace("cite ","\cite",$line);
 	$line = str_replace("date","\date",$line);
+	$line = str_replace("dot","\dot",$line);
+	$line = str_replace("ddot","\ddot",$line);
 	$line = str_replace("footnote","\footnote",$line);
-	echo "<p> formatted line:".$line."</p>";
+	$line = str_replace("frac","\frac",$line);
+	$line = str_replace("latex","\latex",$line);
+	$line = str_replace("dfrac","\dfrac",$line);
+	$line = str_replace("ref","\ref",$line);
+	$line = str_replace("label","\label",$line);
+	$line = str_replace("section","\section",$line);
+	$line = str_replace("subsection","\subsection",$line);
+	$line = str_replace("subsubsection","\subsubsection",$line);
+	//replace double //
+	$line = str_replace("\\\\","\\");
+	
+	//echo "<p> formatted line:".$line."</p>";
 	array_push($output,$line);
 }
 echo implode("\n",$output);
