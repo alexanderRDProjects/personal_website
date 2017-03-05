068484fff6d34d5826978ec877c4d8709047f47d
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 20:37:23 +0000

    building convertor

--- SLtex/convert.php
+++ SLtex/convert.php
@@ -2,5 +2,13 @@
 $content = urldecode($_GET["contents"]);
 $output = [];
 var_dump(array_filter(explode("\n",$content)));
+foreach($input as &$line) {
+	$line = str_replace("pi","\pi",$line);
+	$line = str_replace("phi","\phi",$line);
+	$line = str_replace("cite ","\cite",$line);
+	$line = str_replace("date","\date",$line);
+	$line = str_replace("footnote","\footnote",$line);
+	$output = explode("<>",implode("<>",output)+"<>"+$line);
+}
 echo implode("\n",$output);
 ?>
