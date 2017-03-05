254bfa6cf458767cd609565682560a52b32a26a5
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 02:27:09 +0000

    adapting SLtex

--- SLtex/down.php
+++ SLtex/down.php
@@ -1,15 +1,17 @@
 <?php
+$name = $_GET["name"];
+$contents = urldecode($_GET["contents"});
 
-$str = "Some pseudo-random
-text spanning
-multiple lines";
+//$str = "Some pseudo-random
+//text spanning
+//multiple lines";
 
-header('Content-Disposition: attachment; filename="sample.txt"');
+header('Content-Disposition: attachment; filename="'.$name.'.tex"');
 header('Content-Type: text/plain'); # Don't use application/force-download - it's not a real MIME type, and the Content-Disposition header is sufficient
-header('Content-Length: ' . strlen($str));
+header('Content-Length: ' . strlen($contents));
 header('Connection: close');
 
 
-echo $str;
+echo $contents;
 exit;
 ?>
