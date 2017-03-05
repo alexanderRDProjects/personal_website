352bc05eecacc8465394718f67e51aab260000d4
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 19:18:31 +0000

    improving front end

--- SLtex/index.php
+++ SLtex/index.php
@@ -3,6 +3,20 @@
 <title>SLtex Converter</title>
 </head>
 <body>
+<script>
+function CLatex () {
+	alert("convert to Latex");
+}
+function DLatex () {
+	alert("download Latex");
+}
+function CLatex () {
+	alert("convert to SLTex");
+}
+function CLatex () {
+	alert("download SLTex");
+}
+</script>
 <center>
 <table width = 100%>
 <tr width = 100%>
@@ -29,10 +43,10 @@ Enter Your LaTeX here ...
 </table>
 <table width = 100%>
 <tr>
-<td width = 25%><button>Convert To LaTex</button></td>
-<td width = 25%><button>Download LaTex </button></td>
-<td width = 25%><button>Convert TO SLTex </button></td>
-<td width = 25%><button>Download SLTex </button></td>
+<td width = 25%><button onclick="CLatex">Convert To LaTex</button></td>
+<td width = 25%><button onclick="DLatex">Download LaTex </button></td>
+<td width = 25%><button onclick="CSLTex">Convert TO SLTex </button></td>
+<td width = 25%><button onclick="DSLTex">Download SLTex </button></td>
 </tr>
 </table>
 </center>
