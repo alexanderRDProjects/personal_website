14be2d2c778d43a9a86686c668f42c8cc787ff55
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 02:19:00 +0000

    stil fixing SLtex

--- SLtex/down.php
+++ SLtex/down.php
@@ -1,19 +1,12 @@
 <?php
 
-$mushrooms = 'disjusting';
-$thefile = 'example.tex';
-
-/*if (file_exists($thefile)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($thefile).'"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($thefile));
-    readfile($thefile);
-    exit;
-}
-*/
-echo $thefile;
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="example.tex"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: 1');
+echo "a";
+exit;
 ?>
