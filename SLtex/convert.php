9f4f1754122ed23e41472ed2729efb8d29e85934
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 20:50:50 +0000

    building

--- SLtex/convert.php
+++ SLtex/convert.php
@@ -4,14 +4,15 @@ $output = [];
 $input = array_filter(explode("\n",$content));
 var_dump($input);
 foreach($input as &$line) {
-	echo "<p> unformatted line :"+$line+"</p>";
+	echo "<p> unformatted line :".$line."</p>";
 	$line = str_replace("pi","\pi",$line);
 	$line = str_replace("phi","\phi",$line);
 	$line = str_replace("cite ","\cite",$line);
 	$line = str_replace("date","\date",$line);
 	$line = str_replace("footnote","\footnote",$line);
-	echo "<p> formatted line:"+$line+"</p>";
-	$output = explode("<>",implode("<>",output)+"<>"+$line);
+	echo "<p> formatted line:".$line."</p>";
+	array_push($output,$line);
 }
 echo implode("\n",$output);
 ?>
+	
