bb234f510c136705f48fee107bd5b9f06ca1bf90
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-05 01:32:49 +0000

    fixing SLtex

--- SLtex/download.php
+++ SLtex/download.php
@@ -1,15 +1,15 @@
 <?php
-$file = 'example.tex';
+$thefile = 'example.tex';
 
-if (file_exists($file)) {
+if (file_exists($thefile)) {
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Content-Disposition: attachment; filename="'.basename($thefile).'"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    readfile($file);
+    header('Content-Length: ' . filesize($thefile));
+    readfile($thefile);
     exit;
 }
 ?> 
