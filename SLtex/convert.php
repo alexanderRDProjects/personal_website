38827e05c449b6001c8de541ac1de06cdb9ec50e
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-06 13:05:15 +0000

    improving

--- SLtex/convert.php
+++ SLtex/convert.php
@@ -5,21 +5,21 @@ $input = array_filter(explode("\n",$content));
 var_dump($input);
 foreach($input as &$line) {
 	echo "<p> unformatted line :".$line."</p>";
-	$line = str_replace("pi","\pi",$line);
-	$line = str_replace("phi","\phi",$line);
-	$line = str_replace("cite ","\cite",$line);
-	$line = str_replace("date","\date",$line);
-	$line = str_replace("dot","\dot",$line);
-	$line = str_replace("ddot","\ddot",$line);
-	$line = str_replace("footnote","\footnote",$line);
-	$line = str_replace("frac","\frac",$line);
-	$line = str_replace("latex","\latex",$line);
-	$line = str_replace("dfrac","\dfrac",$line);
-	$line = str_replace("ref","\ref",$line);
-	$line = str_replace("label","\label",$line);
-	$line = str_replace("section","\section",$line);
-	$line = str_replace("subsection","\subsection",$line);
-	$line = str_replace("subsubsection","\subsubsection",$line);
+	$line = str_replace("pi","\\pi",$line);
+	$line = str_replace("phi","\\phi",$line);
+	$line = str_replace("cite ","\\cite",$line);
+	$line = str_replace("date","\\date",$line);
+	$line = str_replace("dot","\\dot",$line);
+	$line = str_replace("ddot","\\ddot",$line);
+	$line = str_replace("footnote","\\footnote",$line);
+	$line = str_replace("frac","\\frac",$line);
+	$line = str_replace("latex","\\latex",$line);
+	$line = str_replace("dfrac","\\dfrac",$line);
+	$line = str_replace("ref","\\ref",$line);
+	$line = str_replace("label","\\label",$line);
+	$line = str_replace("section","\\section",$line);
+	$line = str_replace("subsection","\\subsection",$line);
+	$line = str_replace("subsubsection","\\subsubsection",$line);
 	//replace double //
 	$line = str_replace("\\\\","\\");
 	
