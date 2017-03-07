f5e6846f18322c980ae05aa88681c76ddb287e63
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-07 14:23:44 +0000

    improving

--- SLtex/convert.php
+++ SLtex/convert.php
@@ -31,6 +31,7 @@ foreach($input as &$line) {
 		$head = implode(" ",$words);
 		$head = str_replace("{ ","{",$head);
 		$head = str_replace("} ","}",$head);
+		$line = "";
 		array_push($output,$head);
 	}
 }
@@ -43,76 +44,76 @@ foreach($input as &$line) {
 	//first check if header
 	//consider first word
 	$words = array_filter(explode(" ",$line));
-	if ($words[0] != "author"){
-		if ($words[0] != "title") {
-			echo $words[0],"\n";
-			if ($words[0] == "section"){
-				$words[0] = "\section{";
-				array_push($words,"}");
-			}
-			if ($words[0] == "subsection"){
-				$words[0] = "\subsection{";
-				array_push($words,"}");
-			}
-			$line = implode(" ",$words);
-			if (strpos($line, '$$') !== false){
-				$math_mode = !$math_mode;
-			}
-			//echo "<p> unformatted line :".$line."</p>";
-			if ($math_mode){
-				$line = str_replace("pi","\\pi",$line);
-				$line = str_replace("phi","\\phi",$line);
-			}
-			else{
-				$line = str_replace("pi","$\\pi$",$line);
-				$line = str_replace("phi","$\\phi$",$line);
-			}
-			$line = str_replace("cite ","\\cite",$line);
-			$line = str_replace("date","\\date",$line);
-			$line = str_replace("dot","\\dot",$line);
-			$line = str_replace("ddot","\\ddot",$line);
-			$line = str_replace("footnote","\\footnote",$line);
-			$line = str_replace("frac","\\frac",$line);
-			$line = str_replace("latex","\\latex",$line);
-			$line = str_replace("dfrac","\\dfrac",$line);
-			$line = str_replace("ref","\\ref",$line);
-			$line = str_replace("label","\\label",$line);
-			//$line = str_replace("section","\\section",$line);
-			$line = str_replace("subsection","\\subsection",$line);
-			$line = str_replace("subsubsection","\\subsubsection",$line);
-			$line = str_replace("{ ","{",$line);
-			$line = str_replace(" }","}",$line);
-			//replace double //
-			$line = str_replace("\\\\","\\",$line);
-			if (strpos($line, "$\\$\\") !== false){
-				$line = str_replace("$\\$\\","$\\",$line);
-				$line = str_replace("$$","$",$line);
-			}
-			if (strpos($line, "$*") !== false){
-				$line = explode(" ",$line);
-				foreach ($line as &$word){
-					if (strpos($word, "$*") !== false){
-						$word = str_replace("$*","",$word);
-						$word = str_replace("$\\","",$word);
-					}
-				}
-				$line = implode(" ",$line);
-			}
-			if (strpos($line, "*" !== false)) {
-				if ($math_mode == false) {
-					$line = explode(" ",$line);
-					foreach ($line as &$word){
-						if (strpos($word, "*") !== false){
-							$word = str_replace("*","",$word);
-							$word = str_replace("\\","",$word);
-						}
-					}
-					$line = implode(" ",$line);
-				}
+
+
+	echo $words[0]
+	if ($words[0] == "section"){
+		$words[0] = "\section{";
+		array_push($words,"}");
+	}
+	if ($words[0] == "subsection"){
+		$words[0] = "\subsection{";
+		array_push($words,"}");
+	}
+	$line = implode(" ",$words);
+	if (strpos($line, '$$') !== false){
+		$math_mode = !$math_mode;
+	}
+	//echo "<p> unformatted line :".$line."</p>";
+	if ($math_mode){
+		$line = str_replace("pi","\\pi",$line);
+		$line = str_replace("phi","\\phi",$line);
+	}
+	else{
+		$line = str_replace("pi","$\\pi$",$line);
+		$line = str_replace("phi","$\\phi$",$line);
+	}
+	$line = str_replace("cite ","\\cite",$line);
+	$line = str_replace("date","\\date",$line);
+	$line = str_replace("dot","\\dot",$line);
+	$line = str_replace("ddot","\\ddot",$line);
+	$line = str_replace("footnote","\\footnote",$line);
+	$line = str_replace("frac","\\frac",$line);
+	$line = str_replace("latex","\\latex",$line);
+	$line = str_replace("dfrac","\\dfrac",$line);
+	$line = str_replace("ref","\\ref",$line);
+	$line = str_replace("label","\\label",$line);
+	//$line = str_replace("section","\\section",$line);
+	$line = str_replace("subsection","\\subsection",$line);
+	$line = str_replace("subsubsection","\\subsubsection",$line);
+	$line = str_replace("{ ","{",$line);
+	$line = str_replace(" }","}",$line);
+	//replace double //
+	$line = str_replace("\\\\","\\",$line);
+	if (strpos($line, "$\\$\\") !== false){
+		$line = str_replace("$\\$\\","$\\",$line);
+		$line = str_replace("$$","$",$line);
+	}
+	if (strpos($line, "$*") !== false){
+		$line = explode(" ",$line);
+		foreach ($line as &$word){
+			if (strpos($word, "$*") !== false){
+				$word = str_replace("$*","",$word);
+				$word = str_replace("$\\","",$word);
 			}
 		}
+		$line = implode(" ",$line);
 	}
-	
+	if (strpos($line, "*" !== false)) {
+		if ($math_mode == false) {
+			$line = explode(" ",$line);
+			foreach ($line as &$word){
+				if (strpos($word, "*") !== false){
+					$word = str_replace("*","",$word);
+					$word = str_replace("\\","",$word);
+				}
+			}
+			$line = implode(" ",$line);
+		}
+	}
+
+
+
 	
 	//echo "<p> formatted line:".$line."</p>";
 	array_push($output,$line);
