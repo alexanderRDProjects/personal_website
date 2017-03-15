eb3882a90323dd36e070a01b1535b88050a3c5d0
Alexander Roberts Dunn <ar560@exeter.ac.uk>
2017-03-15 23:28:17 +0000

    figure fixes

--- SLtex/convert.php
+++ SLtex/convert.php
@@ -82,6 +82,7 @@ foreach($input as &$line) {
 		}
 		$words[0] = "\\begin{figure}[h!]\n\\includegraphics[scale=0.4]{".$path.".png}\n\\label{".$path."}\n\\caption{";
 		array_push($words,"}\n\\end{figure}");
+		var_dump($words);
 	}
 	$line = implode(" ",$words);
 	if (strpos($line, '$$') !== false){
