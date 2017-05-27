6df18b4d53de509741a8cbebc00ca491f0799997
Alexander <ar560@exeter.ac.uk>
2017-05-27 21:13:20 +0100

    added basic routing script

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 $url = $_GET["url"];
 echo $url;
+if ($url == "") {
+	//index page 
+	include("template.php");
+	echo "<h1> index </h1> </body></html>"
+} else {
+	include("template.php");
+	echo "<h1> Not found </h1> </body></html>"
 ?>
