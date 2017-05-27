0ac3842d06a3e8d1ecb6def349fbc02e86aba511
Alexander <ar560@exeter.ac.uk>
2017-05-27 21:14:26 +0100

    fixed semicolon error

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ echo $url;
 if ($url == "") {
 	//index page 
 	include("template.php");
-	echo "<h1> index </h1> </body></html>"
+	echo "<h1> index </h1> </body></html>";
 } else {
 	include("template.php");
-	echo "<h1> Not found </h1> </body></html>"
+	echo "<h1> Not found </h1> </body></html>";
 ?>
