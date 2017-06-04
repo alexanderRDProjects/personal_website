8600aa89dda66e758a0ee7d49fa28d4d03a2a3f6
Alexander <ar560@exeter.ac.uk>
2017-06-04 23:07:51 +0100

    added mail script

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+$to      = 'contact@alexanderrdprojects.com';
+$subject = 'Automated Email';
+$message = 'This is a automated email please do not reply';
+$headers = 'From: no-reply@alexanderrdprojects.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
