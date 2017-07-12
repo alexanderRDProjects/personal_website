042caf91de7dacf2b1a4bda3532f61a96bbaeefd
Alexander <ar560@exeter.ac.uk>
2017-07-12 20:29:37 +0100

    formatted 404

--- not_found.php
+++ not_found.php
@@ -1,11 +1,13 @@
 <div class="jumbotron text-center" style="
     background-color: rgba(238,238,238,0.5);
 ">
+<script>
+document.body.style.overflowY = "hidden";​
+</script>
+<div style="
+    padding-bottom: 350px;
+"></div>
 <h1> Page not Found </h1>
-<h4><br></h4>
-<p><br></p>
-<h2> Sorry the page you requested cannot be found please use the nagivation bar to located your page </h2>
-<p><br></p>
 </div>
 <div style='position:absolute;z-index:-15;left:0;top:0;width:100%;height:100%'>
   <img src='seagull.jpg' style='width:100%;height:100%' alt='[]' />
