ead48817d66199d0b6f57a8af2b2129ab4754ad9
Alexander <ar560@exeter.ac.uk>
2017-07-12 20:16:15 +0100

    added 404

---
+++ not_found.php
@@ -0,0 +1,12 @@
+<div class="jumbotron text-center" style="
+    background-color: rgba(238,238,238,0.5);
+">
+<h1> Page not Found </h1>
+<h4><br></h4>
+<p><br></p>
+<h2> Sorry the page you requested cannot be found please use the nagivation bar to located your page </h2>
+<p><br></p>
+</div>
+<div style='position:absolute;z-index:-15;left:0;top:0;width:100%;height:100%'>
+  <img src='background.jpg' style='width:100%;height:100%' alt='[]' />
+</div>
